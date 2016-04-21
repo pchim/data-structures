45c030ab223e3a86114ec6526ce47e05dac3f9e9
pchim <pchim24@gmail.com>
2016-04-21 10:16:17 -0700

    Pass all tests pseudoclassical stack: sprint-one complete

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
 };
 
-
+var stackMethods = {
+  size: function() {
+    return this.length;
+  },
+  push: function(val) {
+    this.storage[this.length] = val;
+    this.length++;
+  },
+  pop: function() {
+    if(this.length > 0) {
+      var result = this.storage[this.length - 1];
+      this.length--;
+      return result;
+    }
+  }
+};
+Stack.prototype.size = stackMethods.size;
+Stack.prototype.push = stackMethods.push;
+Stack.prototype.pop = stackMethods.pop;
