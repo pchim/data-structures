32049cef901fff828c65f70ee96c468c874793ae
pchim <pchim24@gmail.com>
2016-04-21 16:10:15 -0700

    Pass all tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (list.head === null && list.tail === null){
+      list.tail = node;
+      list.head = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var result;
+    if (list.head !== null){
+      result = list.head;
+      list.head = list.head.next;
+    } 
+    return result.value;
   };
 
   list.contains = function(target) {
+
+    var checkItem = function(currentNode, target) {
+      if (currentNode.value === target) {
+        return true;
+      } else if (currentNode.next !== null) {
+        return checkItem(currentNode.next, target);
+      } else {  
+        return false;
+      }     
+    };
+
+    return checkItem(list.head, target);
   };
+
 
   return list;
 };
