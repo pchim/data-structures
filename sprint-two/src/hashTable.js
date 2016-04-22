982a0e82a94180d3d026aedc6722c2c1cbd61450
pchim <pchim24@gmail.com>
2016-04-22 11:31:46 -0700

    Pass all tests hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,47 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var tuple = [k, v];
+
+  if ( Array.isArray(bucket) ){
+    var change = false;
+    
+    for (var i = 0; i < bucket.length; i++){
+      if (bucket[i][0] === k){
+        change = true;
+        bucket[i][1] = v;
+      } 
+    }
+    if (!change){
+      bucket.push(tuple);
+    } 
+
+  } else {
+    this._storage.set(index, [tuple]);
+  }
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++){
+    if (bucket[i][0] === k){
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++){
+    if (bucket[i][0] === k){
+      bucket[i][1] = undefined;
+    } 
+  }
+
 };
 
 
