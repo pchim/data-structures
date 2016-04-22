ca8c6208b1927f06862a5f5f0d57b2062442a187
pchim <pchim24@gmail.com>
2016-04-21 18:16:27 -0700

    Pass all test for tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+
+
+  var checkTree = function(node, target){
+    if (node.value === target){
+      return true;
+    } 
+    if (node.children.length > 0){
+      for (var i = 0; i < node.children.length; i++){
+        var result;
+        // checkTree(node.children[i], target);
+        result = checkTree(node.children[i], target);
+        if(result) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  // debugger;
+  return checkTree(this, target);
+
 };
 
 
@@ -21,3 +45,16 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+  // var checkTree = function(node, target){
+  //   if (node.value === target){
+  //     return true;
+  //   } else if (node.children.length > 0){
+  //     for (var i = 0; i < node.children.length; i++){
+  //       return checkTree(node.children[i], target);
+  //     }
+  //   } else return false;
+  // };
